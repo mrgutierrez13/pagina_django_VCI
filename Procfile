306bcbda83071fda3497076a494c1f545a7ba49e
Moises Gutierrez <rumoigut@hotmail.com>
2020-09-02 12:54:54 -0400

    up2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn locallibrary.wsgi --log-file -
+web: gunicorn paginaVCI.wsgi --log-file -
--- paginaVCI/settings.py
+++ paginaVCI/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'informacion',
     'asesora',
     'colorfield',
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
